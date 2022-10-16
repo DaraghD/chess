fbf668eb83a522bd00eb839153a111b0d7e19447
Mark Naughton <mark@marknaughton.com>
2022-10-16 14:35:38 +0100

    Add match statment to check input piece

--- src/main.py
+++ src/main.py
@@ -1,21 +1,33 @@
 import display
+import pprint
 import user
 import move
 
 def main():
      board = display.generate()
+     p1moved = 0
      print(move.validateMoveWP(6,0,5,1,board))
 
-     pfile = int(input("Enter the file for piece to move:"))
-     prank = int(input("Enter the rank for piece to move:"))
+     while p1moved == 0:
+         pfile = int(input("Enter the file for piece to move:"))
+         prank = int(input("Enter the rank for piece to move:"))
 
-     mfile = int(input("Enter the file for square to move:"))
-     mrank = int(input("Enter the rank for square to move:"))
+         mfile = int(input("Enter the file for square to move:"))
+         mrank = int(input("Enter the rank for square to move:"))
 
 
-     if move.validateMoveWP(pfile, prank, mfile, mrank, board):
-         print("done")
-         board[pfile][prank] = board[mfile][mrank]
+         match board[pfile][prank]:
+             case "WP":
+                 if move.validateMoveWP(pfile, prank, mfile, mrank, board):
+                     print("done")
+                     board[mfile][mrank] = board[pfile][prank]
+                     board[pfile][prank] = 0
+                     p1moved = 1
+                 else:
+                     print("Invalid move")
+
+     pp = pprint.PrettyPrinter(indent=4)
+     pp.pprint(board)
 
 main()
 
