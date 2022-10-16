8d635b6bd3eb986b6dcc8fb9f27d7975e891293b
DaraghD <daraghdownes12@gmail.com>
2022-10-16 14:33:05 +0100

    validate black move function

--- src/move.py
+++ src/move.py
@@ -19,3 +19,25 @@ def validateMoveWP(pos1row, pos1column, pos2row, pos2column, board):
                 return False
     else:
         return False
+
+def validateMoveBP(pos1row, pos1column, pos2row, pos2column, board):
+    flagleftB = False
+    flagrightB = False
+    if board[pos1row][pos1column] == "BP":
+        if board[pos1row+1][pos1column-1] == "WP":
+            flagleftB = True
+        if board[pos1row+1][pos1column+1] == "WP":
+            flagrightB = True
+        if board[pos2row][pos2column] == 0:
+            if pos2row == pos1row+1 and pos2column == pos1column:
+                return True
+    if flagleftB or flagrightB:
+        if board[pos2row][pos2column] == board[pos1row +1][pos1column-1] and flagleftB:
+            return True
+        else:
+            if board[pos2row][pos2column] == board[pos1row +1][pos1column +1] and flagrightB:
+                return True
+            else:
+                return False
+    else:
+        return False
