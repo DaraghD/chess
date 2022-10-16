a9405571bced228d9ccdfc1b6fc3c673b535eefc
DaraghD <daraghdownes12@gmail.com>
2022-10-16 14:05:51 +0100

    validatemoveWP - capture logic, not first move

--- src/move.py
+++ src/move.py
@@ -1,7 +1,20 @@
+
 def validateMoveWP(pos1row, pos1column, pos2row, pos2column, board):
+    flagleft = False
+    flagright = False
     if board[pos1row][pos1column] == "WP":
+        if board[pos1row-1][pos1column-1] =="BP":
+            flagleft = True
+        if board[pos1row-1][pos1column+1] == "BP":
+            flagright = True
         if board[pos2row][pos2column] == 0:
             if pos2row == pos1row - 1 and pos2column == pos1column:
                 return True
     else:
-        return False
+        if board[pos2row][pos2column] == board[pos1row-1][pos1column-1] and flagleft == True:
+                return True
+        else:
+            if board[pos2row][pos2column] == board[pos1row-1][pos1column+1] and flagright == True:
+                return True
+            else:
+                return False
